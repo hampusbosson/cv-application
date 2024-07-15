9d9da5be1f127b62d744edaf1fd3e9eb4b57469e
Hampus Bosson <hampus.bosson@outlook.com>
2024-07-15 14:10:47 +0200

    Add content and style the header for the resume side.

--- src/components/ResumeSide/ResumeSide.jsx
+++ src/components/ResumeSide/ResumeSide.jsx
@@ -1,10 +1,33 @@
 import './ResumeSide.css'
+import icons from '../../assets/icons/Icons';
+
+function Header() {
+    return (
+        <div className='resume-header'>
+            <h1 className='header-name'>Example Name</h1>
+            <div className='header-info'>
+                <div className='mail-info'>
+                    {icons.enevelope}
+                    <p>example.email@mail.co.uk</p>
+                </div>
+                <div className='phone-info'>
+                    {icons.phone}
+                    <p>+46 12-345 67 89</p>
+                </div>
+                <div className='location-info'>
+                    {icons.location}
+                    <p>Stockholm, SWE</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 function ResumeSide() {
     return (
-        <div className='resume-side'>
-            <p>right side</p>
-        </div>
+        <section className='resume-side'>
+            <Header />
+        </section>
     )
 }
 
